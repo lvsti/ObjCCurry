387ef7d5445063720df4ed6132ea1f29231b2043
Lk Xf <elveestei@gmail.com>
2014-04-27 19:45:46 +0200

    Initial Commit

---
+++ ObjCCurry/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  ObjCCurry
+//
+//  Created by Tamas Lustyik on 2014.04.27..
+//  Copyright (c) 2014 LKXF. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
